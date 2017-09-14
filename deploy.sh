ff8befe353039a85f35c8dc38c36eef26fd3d2e5
Fred Vassard <fred@studyblue.com>
2017-09-14 11:58:53 -0700

    $@

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,10 @@ git add .
 git commit -am '$@'
 git push
 ./pushtag.sh
+VERSION=$(git describe --abbrev=0 --tags)
 echo "Creating Source"
 python2 ./setup.py sdist
 echo "Creating Wheel"
 python2 ./setup.py bdist_wheel
 echo "Uploading to pypi"
-twine upload dist/*
+twine upload dist/*-${VERSION}*
